b40f0576c7e8db8ed8a11891e54baf9a3ec38da2
yuvsag <59389950+yuvsag@users.noreply.github.com>
2020-01-07 12:50:51 +0200

    more windows

---
+++ ClimateControll/ArduConnect.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ClimateControll
+{
+    partial class ArduConnect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPortName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.data = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "port:";
+            // 
+            // tbPortName
+            // 
+            this.tbPortName.Location = new System.Drawing.Point(79, 47);
+            this.tbPortName.Name = "tbPortName";
+            this.tbPortName.Size = new System.Drawing.Size(100, 20);
+            this.tbPortName.TabIndex = 9;
+            this.tbPortName.Text = "COM";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // data
+            // 
+            this.data.AutoSize = true;
+            this.data.Location = new System.Drawing.Point(127, 252);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(52, 13);
+            this.data.TabIndex = 6;
+            this.data.Text = "Waiting...";
+            // 
+            // ArduConnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPortName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.data);
+            this.Name = "ArduConnect";
+            this.Text = "ArduConnect";
+            //this.Load += new System.EventHandler(this.ArduConnect_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPortName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label data;
+    }
+}
--- ClimateControll/SettingsFRM.cs
+++ ClimateControll/SettingsFRM.cs
@@ -16,5 +16,71 @@ namespace ClimateControll
         {
             InitializeComponent();
         }
+
+        private void SettingsFRM_Load(object sender, EventArgs e)
+        {
+            TbMaxTemp.Text = "" + Properties.Settings.Default.maxTemp;
+            TbMinTemp.Text = "" + Properties.Settings.Default.minTemp;
+            TbMaxHumidity.Text = "" + Properties.Settings.Default.maxHumidity;
+            TbMinHumidity.Text = "" + Properties.Settings.Default.minHumidity;
+            TbMailAdress.Text = Properties.Settings.Default.mailAdrees;
+            CbMail.Checked = Properties.Settings.Default.sendMail;
+            CbAlert.Checked = Properties.Settings.Default.turnOnAlert;
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            bool currect = true;
+            int cheak;
+            label1.ForeColor = System.Drawing.Color.Black;
+            label2.ForeColor = System.Drawing.Color.Black;
+            label3.ForeColor = System.Drawing.Color.Black;
+            label4.ForeColor = System.Drawing.Color.Black;
+
+            if (!int.TryParse(TbMinTemp.Text, out cheak))
+            {
+                currect = false;
+                label1.ForeColor = System.Drawing.Color.Red;
+            }
+
+            if (!int.TryParse(TbMaxTemp.Text, out cheak))
+            {
+                currect = false;
+                label2.ForeColor = System.Drawing.Color.Red;
+            }
+
+            if (!int.TryParse(TbMinHumidity.Text, out cheak))
+            {
+                currect = false;
+                label3.ForeColor = System.Drawing.Color.Red;
+            }
+
+            if (!int.TryParse(TbMaxHumidity.Text, out cheak))
+            {
+                currect = false;
+                label4.ForeColor = System.Drawing.Color.Red;
+            }
+
+            if (currect && (TbMailAdress.Text != "" || CbMail.Checked == false))
+            {
+                Properties.Settings.Default.maxTemp = int.Parse(TbMaxTemp.Text);
+                Properties.Settings.Default.minTemp = int.Parse(TbMinTemp.Text);
+                Properties.Settings.Default.maxHumidity = int.Parse(TbMaxHumidity.Text);
+                Properties.Settings.Default.minHumidity = int.Parse(TbMinHumidity.Text);
+                Properties.Settings.Default.mailAdrees = TbMailAdress.Text;
+                Properties.Settings.Default.sendMail = CbMail.Checked;
+                Properties.Settings.Default.turnOnAlert = CbAlert.Checked;
+                Properties.Settings.Default.Save();
+                MessageBox.Show("all values are saved");
+            }
+            else if (TbMailAdress.Text == "" && CbMail.Checked == true)
+            {
+                MessageBox.Show("email adress box is empty", "error");
+            }
+            else
+            {
+                MessageBox.Show("values in red are bed", "error");
+            }
+        }
     }
 }

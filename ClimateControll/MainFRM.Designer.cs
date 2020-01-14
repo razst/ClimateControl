c0e7c9e84465ca6fdf459ba42e0c6231f6f27961
yuvsag <yy159753ss>
2020-01-14 11:21:58 +0200

    fix project issues

---
+++ ClimateControll/MainFRM.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ClimateControll
+{
+    partial class MainFRM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.config = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // config
+            // 
+            this.config.Location = new System.Drawing.Point(485, 382);
+            this.config.Name = "config";
+            this.config.Size = new System.Drawing.Size(75, 23);
+            this.config.TabIndex = 1;
+            this.config.Text = "open";
+            this.config.UseVisualStyleBackColor = true;
+            this.config.Click += new System.EventHandler(this.config_Click);
+            // 
+            // MainFRM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.config);
+            this.Controls.Add(this.button1);
+            this.Name = "MainFRM";
+            this.Text = "Climate Controll System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button config;
+    }
+}
+

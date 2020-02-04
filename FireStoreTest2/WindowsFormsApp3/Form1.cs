6bdad34e738a879bc303d23afcadcb0b0e88b58e
Amichay32 <59389950+Amichay32@users.noreply.github.com>
2020-02-04 12:45:29 +0200

    Firestore test project

---
+++ FireStoreTest2/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Firestore;
+
+namespace WindowsFormsApp3
+{
+
+    public partial class Form1 : Form
+    {
+        private FirestoreDb db = FirestoreDb.Create("climatehistory-3ff7e");
+        private string COLLECTION_NAME = "ClimateInfo";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            long unixTime = ((DateTimeOffset)now).ToUnixTimeSeconds();
+
+            TempInfo t = new TempInfo();
+            t.WhenUNIX = unixTime;
+            t.WhenString = now.ToString("yyyy-MM-dd HH:mm:ss");
+            t.Temperature = 10;
+            t.Humidity = 70;
+
+            DocumentReference docRef = db.Collection(COLLECTION_NAME).Document(t.WhenUNIX.ToString());
+            
+            
+            await docRef.SetAsync(t);
+
+        }
+    }
+}
---
+++ FireStoreTest2/WindowsFormsApp3/TempInfo.cs
@@ -0,0 +1,19 @@
+﻿using Google.Cloud.Firestore;
+
+namespace WindowsFormsApp3
+{
+    [FirestoreData]
+
+    internal class TempInfo
+    {
+        [FirestoreProperty]
+        public string WhenString { get; set; }
+        [FirestoreProperty]
+        public long WhenUNIX { get; set; }
+        [FirestoreProperty]
+        public float Temperature { get; set; }
+        [FirestoreProperty]
+        public int Humidity { get; set; }
+
+    }
+}

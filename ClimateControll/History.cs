238f3252731482d2de249efcc7b639bd64d0e8ab
Amichay32 <59389960+Amichay32@users.noreply.github.com>
2020-02-18 12:22:03 +0200

    new history table

--- ClimateControll/History.cs
+++ ClimateControll/History.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Cloud.Firestore;
+
+
 
 namespace ClimateControll
 {
@@ -15,6 +18,28 @@ namespace ClimateControll
         public History()
         {
             InitializeComponent();
+        }
+
+        private async void History_Load(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("time", typeof(string));
+            table.Columns.Add("temp", typeof(float));
+            table.Columns.Add("hum", typeof(float));
+            dataGridView1.DataSource = table;
+
+
+            Query capitalQuery = MainFRM.db.Collection(MainFRM.COLLECTION_NAME).OrderByDescending("WhenUNIX").Limit(10);
+            QuerySnapshot capitalQuerySnapshot = await capitalQuery.GetSnapshotAsync();
+            foreach (DocumentSnapshot documentSnapshot in capitalQuerySnapshot.Documents)
+            {
+                TempInfo temp = documentSnapshot.ConvertTo<TempInfo>();
+
+                table.Rows.Add(temp.WhenString, temp.Temperature,temp.Humidity);
+
+            }
+
+
         }
     }
 }
